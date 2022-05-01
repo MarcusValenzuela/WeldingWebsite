240c827febb3b5454e3e4bc46994b40c0e738b07
unadulterateddesign <marcus@elevatesoftware.io>
2022-05-01 01:49:52 -0700

    third commit

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,5 +1,20 @@
 import React from "react";
-import { FooterContainer, FooterLink, FooterLinkItems, FooterLinksContainer, FooterLinksWrapper, FooterLinkTitle, FooterWrap } from "./FooterElements";
+import {FaFacebook, FaInstagram, FaTwitter, FaLinkedin} from 'react-icons/fa'
+import {
+  FooterContainer,
+  FooterLink,
+  FooterLinkItems,
+  FooterLinksContainer,
+  FooterLinksWrapper,
+  FooterLinkTitle,
+  FooterWrap,
+  SocialIconLink,
+  SocialIcons,
+  SocialLogo,
+  SocialMedia,
+  SocialMediaWrap,
+  WebsiteRights,
+} from "./FooterElements";
 
 const Footer = () => {
   return (
@@ -12,12 +27,29 @@ const Footer = () => {
 
               <FooterLink to="/">Email: xxxxxxxx@yahoo.com</FooterLink>
               <FooterLink to="/">Phone: xxx-xxx-xxxx</FooterLink>
-              
-
             </FooterLinkItems>
           </FooterLinksWrapper>
         </FooterLinksContainer>
-        
+        <SocialMedia>
+          <SocialMediaWrap>
+            <SocialLogo to="/">Matt Hall</SocialLogo>
+            <WebsiteRights>Piedmont LLC © 2022 All rights reserved</WebsiteRights>
+            <SocialIcons>
+              <SocialIconLink href="//www.facebook.com" target="_blank" aria-label="Facebook">
+                <FaFacebook />
+              </SocialIconLink>
+              <SocialIconLink href="//instagram.com" target="_blank" aria-label="Instagram">
+                <FaInstagram />
+              </SocialIconLink>
+              <SocialIconLink href="//www.twitter.com" target="_blank" aria-label="Twitter">
+                <FaTwitter />
+              </SocialIconLink>
+              <SocialIconLink href="//www.linkedin.com" target="_blank" aria-label="LinkedIn">
+                <FaLinkedin />
+              </SocialIconLink>
+            </SocialIcons>
+          </SocialMediaWrap>
+        </SocialMedia>
       </FooterWrap>
     </FooterContainer>
   );
